3b080a56d06aeb9f3297ad4a4abc8e7003179b5a
HyungjunKim <hjkim1568@naver.com>
2024-03-26 13:09:36 +0900

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -24,7 +24,22 @@ def fix_seed(SEED=42):
     random.seed(SEED)
 
 
+def get_label(demo):
+    # Set case-control patients
+    label = pd.read_csv("/home/hjkim/projects/local_dev/delirium/data/label.csv", index_col=0)
+    label = label[label.patientunitstayid.isin(demo.patientunitstayid)] # demographics 없는 경우 제외 (Age, Gender, Weight/Height)
 
+    case = label[(label.delirium==1) & (label.charttime>1*24*60)] # onset 24h ~ 7days
+    case_plist = list(set(case.patientunitstayid))
+    control = label[(label.delirium==0) & (label.charttime>1*24*60) ]    
+    control = control[~control.patientunitstayid.isin(case_plist)].reset_index(drop=True)  # delirium positive patients excluded
+
+    case = case.sort_values(by=['patientunitstayid', 'charttime']).groupby(['patientunitstayid']).head(1).reset_index(drop=True)
+    control_sampled = control.groupby('patientunitstayid').apply(lambda x: x.sample(min(2, len(x)), random_state=42))
+    control_sampled = control_sampled[['charttime', 'delirium']].reset_index()[['patientunitstayid', 'charttime', 'delirium']]
+    final_label = pd.concat([case, control_sampled], axis=0).reset_index(drop=True)
+
+    return final_label
 
 def loss_plot(train_losses, valid_losses, dst_path):
     plt.clf()
@@ -49,3 +64,41 @@ def auc_plot(train_auc, valid_auc, dst_path):
     plt.legend()
     plt.savefig(dst_path + '/auc_plot.png')  # Loss 그래프를 이미지 파일로 저장
     plt.show()
+
+
+def qSOFA_Score(sys_bp, rr, gcs):
+    score = 0
+
+    if (sys_bp < 100) and (sys_bp > 0):
+        score +=1
+    if (rr > 20):
+        score += 1
+    if (gcs <= 14) and (gcs > 0):
+        score += 1
+    return score
+
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=False, delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+
+    def __call__(self, val_loss):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.counter = 0
