b2b0ebffe44584dc5ea19b93f29ce81b73fad476
HyungjunKim <hjkim1568@naver.com>
2024-03-27 11:09:53 +0900

    Update and rename train.py to train_static.py

--- train_static.py
+++ train_static.py
@@ -1,6 +1,6 @@
-import os
+import os, warnings
 import pandas as pd, numpy as np
-
+warnings.filterwarnings('ignore')
 # Torch
 import torch
 import torch.nn as nn
@@ -12,19 +12,23 @@ from dataset.dataset import Delirium_Dataset
 from utils.util import fix_seed, loss_plot, auc_plot, EarlyStopping
 from models.model import CNNLSTMModel, CRNN, AttentionLSTM
 
-def train_one_epoch(model, dataloader, criterion, optimizer, metric, device, DATA_LENGTH, LEAD_TIME):
+def train_one_epoch(model, dataloader, optimizer, metric, device):
     model.train()
     output_list = []
     target_list = []
 
     running_loss = 0.0
     for batch_idx, (x, y) in enumerate(dataloader):
-        
-        x_emr = x['emr'][:,:].to(device)
-        x_vitals = x['vitals'][:, -DATA_LENGTH:, :].to(device) # VITALS: HR/RR/SpO2/DBP/SBP/BT
-        y = y.to(device)
-        outputs = model(x_emr, x_vitals).to(torch.float16)
+        # For weighted Loss
+        pos_weight = torch.tensor(y.sum() / len(y))
+        criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
+        x_emr = x['emr'][:,:].to(device)
+        x_vitals = x['vitals'][:, :, :].to(device) # [:,144:,:]      # VITALS: HR/RR/SpO2/DBP/SBP/BT
+        y = y.to(device).float()
+        outputs = model(x_emr, x_vitals) 
+        outputs = outputs.float()
+        
         optimizer.zero_grad()
         loss = criterion(outputs, y)
         loss.backward()
@@ -42,7 +46,7 @@ def train_one_epoch(model, dataloader, criterion, optimizer, metric, device, DAT
 
     return final_loss, auc
 
-def valid_one_epoch(model, dataloader, criterion, metric, device, DATA_LENGTH, LEAD_TIME):
+def valid_one_epoch(model, dataloader, metric, device):
     model.eval()
     output_list = []
     target_list = []
@@ -50,13 +54,19 @@ def valid_one_epoch(model, dataloader, criterion, metric, device, DATA_LENGTH, L
 
     with torch.no_grad():
         for batch_idx, (x, y) in enumerate(dataloader):
-            x_emr = x['emr'][:,:].to(device)
-            x_vitals = x['vitals'][:, -DATA_LENGTH:, :].to(device) # [:,144:,:]
-            y = y.to(device)
+            # For weighted Loss
+            pos_weight = torch.tensor(y.sum() / len(y))
+            criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
-            outputs = model(x_emr, x_vitals).to(torch.float16)
+            x_emr = x['emr'][:,:].to(device)
+            x_vitals = x['vitals'][:, :, :].to(device) # [:,144:,:]
+            y = y.to(device).float()
+
+            outputs = model(x_emr, x_vitals)
+            outputs = outputs.float()
 
             loss = criterion(outputs, y)
+
             running_loss += loss.item()
             output_list.extend(outputs.cpu().detach().numpy())
             target_list.extend(y.cpu().detach().numpy())
@@ -72,21 +82,17 @@ def valid_one_epoch(model, dataloader, criterion, metric, device, DATA_LENGTH, L
 
 if __name__ == "__main__":
 # 0. Set Directory Path
-    data_path="/home/hjkim/projects/local_dev/delirium/data/dynamic"
-    dst_path = "/home/hjkim/projects/local_dev/delirium/result"
-    # For 
+    data_path="/home/hjkim/projects/local_dev/delirium/data"
+    dst_path = "/home/hjkim/projects/local_dev/delirium/result_static"
     os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    os.environ["TORCH_USE_CUDA_DSA"] = '1'
-    # torch.cuda.empty_cache()
+
 # 1. Dataset
     BATCH_SIZE=64
-    LEAD_TIME = 2       
-    DATA_LENGTH = 2 * 12   # 1Hour = 12 vitals
     # load npy files
-    train_data = np.load(os.path.join(data_path, "train_" + str(LEAD_TIME) + "h.npy"), allow_pickle=True).item()
-    valid_data = np.load(os.path.join(data_path, "valid_" + str(LEAD_TIME) + "h.npy"), allow_pickle=True).item()
-    test_data = np.load(os.path.join(data_path, "test_" + str(LEAD_TIME) + "h.npy"), allow_pickle=True).item()
+    train_data = np.load(os.path.join(data_path, "train.npy"), allow_pickle=True).item()
+    valid_data = np.load(os.path.join(data_path, "valid.npy"), allow_pickle=True).item()
+    test_data = np.load(os.path.join(data_path, "test.npy"), allow_pickle=True).item()
     # create dataset
     train_dataset = Delirium_Dataset(train_data)
     valid_dataset = Delirium_Dataset(valid_data)
@@ -96,24 +102,30 @@ if __name__ == "__main__":
     valid_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, drop_last=True)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, drop_last=True)
     emr_size = train_data['emr'][:,:].shape[1]
-    vital_size = train_data['vitals'][:, -DATA_LENGTH:, :].shape[2]
+    vital_size = train_data['vitals'][:, :, :].shape[2]
+    print(f"TRAIN SIZE: {len(train_dataloader)}, VALID SIZE: {len(valid_dataloader)}, TEST SIZE: {len(test_dataloader)}")
+    print("LABEL")
+    print(f"TRAIN DELIRIUM: {train_data['label'].sum()}, NON-DELIRIUM :{ len(train_data['label']) - train_data['label'].sum() } \
+          RATE: {train_data['label'].sum() / len(train_data['label']) } ")
+    print(f"VALID DELIRIUM: {valid_data['label'].sum()}, NON-DELIRIUM :{ len(valid_data['label']) - valid_data['label'].sum() }\
+          RATE: {valid_data['label'].sum() / len(valid_data['label']) } ")
+    print(f"TEST DELIRIUM: {test_data['label'].sum()}, NON-DELIRIUM :{ len(test_data['label']) - test_data['label'].sum() } \
+          RATE: {test_data['label'].sum() / len(test_data['label']) } ")
 
 # 2. Set options (device, Loss, etc ....)
     SEED=42
-    NUM_EPOCHS = 300
-    LEARNING_RATE = 1e-5
-    device = torch.device ('cuda' if torch.cuda.is_available() else 'cpu')
+    NUM_EPOCHS = 100
+    LEARNING_RATE = 1e-4
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     criterion = nn.BCEWithLogitsLoss()
-    # criterion = nn.BCELoss()
     metric = BinaryAUROC()
     fix_seed(SEED)
 
     model = CNNLSTMModel(emr_size, vital_size).to(device)
     # model = CRNN(emr_size=emr_size, vitals_size=vital_size).to(device)
     # model = AttentionLSTM(emr_size=emr_size, vitals_size=vital_size).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-3)
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=False)
-
 # 3. Training & Validation
     train_loss_list = []
     valid_loss_list = [] 
@@ -121,20 +133,23 @@ if __name__ == "__main__":
     valid_auc_list = []
 
     # Early stopping
-    early_stopping = EarlyStopping(patience=5, verbose=True)
+    early_stopping = EarlyStopping(patience=10, verbose=True)
 
     for epoch in range(NUM_EPOCHS):
 
-        train_epoch_loss, train_auc = train_one_epoch(model, train_dataloader, criterion, optimizer, metric, device, DATA_LENGTH, LEAD_TIME)  # train
-        val_epoch_loss, val_auc = valid_one_epoch(model, valid_dataloader, criterion, metric, device, DATA_LENGTH, LEAD_TIME) # valid
-        # Scheduler
-        scheduler.step(val_epoch_loss)
+        train_epoch_loss, train_auc = train_one_epoch(model, train_dataloader, optimizer, metric, device)  # train
+        val_epoch_loss, val_auc = valid_one_epoch(model, valid_dataloader, metric, device) # valid
+        
+        early_stopping(val_auc)
 
-        early_stopping(val_epoch_loss)
         if early_stopping.early_stop:
             print("Early stopping")
             best_epoch = epoch
+            best_model = model.state_dict()
             break
+
+        # Scheduler
+        scheduler.step(val_epoch_loss)
 
         # train results
         train_loss_list.append(train_epoch_loss)
@@ -150,7 +165,8 @@ if __name__ == "__main__":
     auc_plot(train_auc_list, valid_auc_list, dst_path=dst_path)
     
     # Testset
-    test_loss, test_auc = valid_one_epoch(model, test_dataloader, criterion, metric, device, DATA_LENGTH, LEAD_TIME) 
+    model.load_state_dict(best_model)
+    test_loss, test_auc = valid_one_epoch(model, test_dataloader, metric, device) 
     print(f"BEST PERFORMANCE: {best_epoch}")
     print(f"Test Loss: {test_loss}, Test AUROC: {test_auc}")
 
